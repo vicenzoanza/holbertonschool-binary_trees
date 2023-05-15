2f171bd0cd6948d328c5d2e7cee9a778c69316fd
vicenzoanza <viceanza2010@hotmail.com>
2023-05-15 06:03:50 -0700

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,19 +3,20 @@
 /**
  *
  *
- **/ 
+ **/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
 	if (node == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 
-	node-> parent = parent;
-	node-> n = value;
-	node-> right = NULL;
-	node-> left = NULL;
+	node->parent = parent;
+	node->n = value;
+	node->right = NULL;
+	node->left = NULL;
 
-	return(node);
+	return (node);
 }
