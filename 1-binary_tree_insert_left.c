e2ab73de16c1b8f84c40c1aabb5a016d97a2f223
vicenzoanza <viceanza2010@hotmail.com>
2023-05-16 06:28:15 -0700

    fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,11 +5,12 @@
  **/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *node;
 
 	if (parent == NULL)
 	return (NULL);
 
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	node = malloc(sizeof(binary_tree_t));
 
 	if (node == NULL)
 	return (NULL);
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,11 +4,12 @@
  **/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *node;
 
 	if (parent == NULL)
 	return (NULL);
 
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	node = malloc(sizeof(binary_tree_t));
 
 	if (node == NULL)
 	return (NULL);
