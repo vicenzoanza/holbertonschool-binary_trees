03277167dd596e73b532100b94a118df76019718
root <5761@holbertonstudents.com>
2023-05-16 15:59:01 -0700

    soblings

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,26 @@
+#include "binary_trees."
+
+binary_tree_t
+*binary_tree_sibling(binary_tree_t *node)
+{
+	int s;
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	s = node->n;
+
+	if (node->parent)
+	{
+		if (node->parent->right && node->parent->left)
+		{
+			if (s > node->parent->left->n)
+			{
+				return (node->parent->left);
+			}
+			return (node->parent->right);
+		}
+	}
+	return (NULL);
+}
